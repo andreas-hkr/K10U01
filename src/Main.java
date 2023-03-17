dabf9a82ddb70c854ba9f43e18f16cc7b9e9aa38
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-17 13:38:44 +0100

    Solved

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,30 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         // Läs in ett filnamn från användaren
         // Öppna filen och läs in X temperaturangivelser (en per rad)
         // Skriv ut den högsta temperaturen och medeltemperaturen
 
+        Scanner input = new Scanner(System.in);
+        System.out.print("Ange ett filnamn: ");
+        String filename = input.nextLine();
 
+        Scanner fileIn = new Scanner(new File(filename));
+        int count = 0;
+        double max = 0;
+        double total = 0;
+
+        while (fileIn.hasNextDouble()) {
+            double value = fileIn.nextDouble();
+            max = Math.max(value, max);
+            total += value;
+            count++;
+        }
+
+        System.out.println("Högsta temperatur: " + max);
+        System.out.println("Medeltemperatur: " + (total/count));
     }
 }
